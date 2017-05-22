853f222c7610049e7ee82721a5e406ab2bc70240
Martin <mj@easir.com>
2017-05-22 14:04:10 +0200

    Implement methods for info about connnected users - Done (explained in Main)

--- src/server/Main.java
+++ src/server/Main.java
@@ -10,9 +10,14 @@ import server.controller.connectionDB.DatabaseConnection;
 import server.controller.connectionREST.ConnectionREST;
 import server.controller.connectionSocket.ServerConnection;
 
+import java.net.Socket;
+
+import static java.lang.Thread.sleep;
+
 public class Main extends Application {
 
     public static DatabaseConnection databaseConnection;
+    public static ServerConnection serverConnection;
     public static ConnectionREST connectionREST;
     public static Gson gson;
 
@@ -26,9 +31,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        //launch(args);
         gson = new Gson();
-        ServerConnection serverConnection = new ServerConnection();
+        serverConnection = new ServerConnection();
         new Thread(serverConnection).start();
         databaseConnection = DatabaseConnection.getDatabaseConnection();
         connectionREST = new ConnectionREST();
@@ -36,5 +40,22 @@ public class Main extends Application {
         System.out.println(connectionREST.getMovie(283995));
         //connectionREST.getRequest("search/movie", "&query=Pe");
         System.out.println("som tu");
+
+
+        //todo Andrea
+        //Wait for running client to show examples (No need later)
+        try {
+            sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //I made methods for you to get all connected clients you call it in your controller like: Main.serverConnection. ....
+        //here is example
+        System.out.println(serverConnection.getClients().size());
+        //print IP of clients
+        for (Socket client : serverConnection.getClients()) {
+            System.out.println(client.getRemoteSocketAddress());
+        }
+        launch(args);
     }
 }
