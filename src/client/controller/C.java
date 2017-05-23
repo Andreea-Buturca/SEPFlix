2aad79cb7fc8b67a56730ccef945858367b29fc1
Marek <marek.lewy23@gmail.com>
2017-05-23 23:42:59 +0200

    Second tier tinkering, working getting trailers from youtube using their api queries.

--- src/client/controller/C.java
+++ src/client/controller/C.java
@@ -1,9 +1,15 @@
 package client.controller;
 import client.Main;
+import com.google.gson.Gson;
+import com.google.gson.internal.StringMap;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
-import java.io.IOException;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 /**
  * Created by Marek on 23-May-17.
@@ -13,11 +19,46 @@ public class C {
     public TextField searchField;
 
     public void Play(ActionEvent actionEvent) throws IOException {
+        String id = getIDfromYT(searchField.getText());
         ytView.getEngine().load(
-                "https://www.youtube.com/results?search_query="+searchField.getText()+" trailer"
+                "https://www.youtube.com/embed/"+id
         );
         ytView.setPrefSize(600, 400);
         Main.stage.show();
+    }
+
+    private String getIDfromYT(String title) throws IOException {
+        String json = readJsonFromUrl(
+                "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+title+"+trailer&type=video&key=AIzaSyDuTI4P28XHLbygh3-50h5TIhPlt3ahAys");
+        Gson gson = new Gson();
+        StringMap<Object> results = gson.fromJson(json, StringMap.class);
+        System.out.println(results);
+        System.out.println(results.get("items"));
+        ArrayList<StringMap<Object>> items = (ArrayList<StringMap<Object>>) results.get("items");
+        StringMap<Object> iditem = (StringMap<Object>) items.get(0).get("id");
+        System.out.println(iditem.get("videoId"));
+        return (String) iditem.get("videoId");
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static String readJsonFromUrl(String url) throws IOException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            //json = new JSONObject(jsonText);
+            return jsonText;
+        } finally {
+            is.close();
+        }
     }
 
 
